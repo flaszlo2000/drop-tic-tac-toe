badbdc4ce7e1ceedbc00049d089d92c1b06d19b4
flaszlo2000 <flaszlo2000@gmail.com>
2020-06-18 17:24:56 +0200

    clumsy ai

--- connect_four/connect_four/ai.c
+++ connect_four/connect_four/ai.c
@@ -14,12 +14,17 @@ char get_char_from_int(int char_in_int_format) {
     return (97 + char_in_int_format);
 }
 
+struct point {
+    int x;
+    int y;
+};
+
 int get_user_point_count(map_data * map, unsigned short needed_number) {
     int c = 0;
 
     for(int i=0; i < map->x; i++) {
         for(int j=0; j < map->y; j++) {
-            if(map->pMap[i][j] == needed_number + '0') {
+            if(map->pMap[i][j] == needed_number) {
                 c++;
             }
         }
@@ -29,27 +34,20 @@ int get_user_point_count(map_data * map, unsigned short needed_number) {
 }
 
 int random_move(int to) {
-    srand(time(NULL));
-
     return (rand() % to);
 }
 
 char ai_main(map_data * map, unsigned short round) {
     char result;
-    unsigned short user_id = round % 2;
 
-    if(round == 1) {
-        // if the ai have to start, then that will choose a random start pos
-        result = get_char_from_int(random_move(map->x));
+    unsigned short user_id = round % 2;
+    int current_point_count = get_user_point_count(map, user_id+1);
+    
+    if(current_point_count > 0) {
+        // atm both has at least a point on the map
+        result = get_char_from_int(random_move(map->x)); // TODO
     } else {
-        int current_point_count = get_user_point_count(map, user_id+1);
-        
-        if(current_point_count > 0) {
-            // atm both has at least a point on the map
-            result = 'b'; // TODO
-        } else {
-            result = get_char_from_int(random_move(map->x));
-        }
+        result = get_char_from_int(random_move(map->x));
     }
 
     return result;
