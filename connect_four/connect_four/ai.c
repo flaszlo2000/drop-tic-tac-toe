1276dec560fd350827b481fa2b68b71b986954d9
flaszlo2000 <flaszlo2000@gmail.com>
2020-06-18 18:22:08 +0200

    check the point

--- connect_four/connect_four/ai.c
+++ connect_four/connect_four/ai.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 char get_char_from_int(int char_in_int_format) {
     return (97 + char_in_int_format);
@@ -24,17 +25,27 @@ int get_user_point_count(map_data * map, unsigned short needed_number) {
 
     for(int i=0; i < map->x; i++) {
         for(int j=0; j < map->y; j++) {
-            if(map->pMap[i][j] == needed_number) {
+            if(map->pMap[i][j] == needed_number + '0') {
                 c++;
             }
         }
     }
+
+    printf("count: %d\n", c);
 
     return c;
 }
 
 int random_move(int to) {
     return (rand() % to);
+}
+
+bool empty_pos(map_data * map, int x, int y) {
+    bool result = false;
+    
+    if(map->pMap[x][y] == '0') { result = true; };
+
+    return result;
 }
 
 char ai_main(map_data * map, unsigned short round) {
@@ -47,7 +58,14 @@ char ai_main(map_data * map, unsigned short round) {
         // atm both has at least a point on the map
         result = get_char_from_int(random_move(map->x)); // TODO
     } else {
-        result = get_char_from_int(random_move(map->x));
+        int random_start_pos;
+        
+        while(true) {
+            random_start_pos = random_move(map->x);
+            if(empty_pos(map, map->y-1, random_start_pos)) { break; }
+        }
+
+        result = get_char_from_int(random_start_pos);
     }
 
     return result;
