2a9c10487a2fc0ada1f60965c15b6ce4deec2f77
flaszlo2000 <flaszlo2000@gmail.com>
2020-06-19 03:28:58 +0200

    bugfix

--- connect_four/connect_four/ai.c
+++ connect_four/connect_four/ai.c
@@ -30,7 +30,9 @@ int get_user_point_count(map_data * map, unsigned short needed_number) {
 }
 
 int random_move(int to) {
-    return (rand() % to);
+    int result = (rand() % to );
+    printf("%d asdasdasdasd!!!!!!!!!!!!!!!!!!!!!4\n", result);
+    return result;
 }
 
 bool empty_pos(map_data * map, int x, int y) {
@@ -58,6 +60,12 @@ char get_winning_opportunity(map_data * map, int needed_char, unsigned char goal
                     // count how many symbols do we have in a line
                     if(map->pMap[k][j] == needed_char + '0') {
                         c++;
+
+                        if(c == goal) {
+                            if(i - goal+1 < 0) { // if this is on the top
+                                c = 0;
+                            }
+                        }
                     } else {
                         if(map->pMap[k][j] != 0 + '0') {
                             // if an other player owns the next place
@@ -111,7 +119,7 @@ char get_winning_opportunity(map_data * map, int needed_char, unsigned char goal
 
             zero_pos = 0;
             // check / (up-right)
-            if(i-goal-1 >= 0 && j+goal-1 < map->y) {
+            if(i-goal+1 >= 0 && j+goal-1 < map->y) {
                 zero_counter = 0;
                 
                 for(int k=0; k < goal; k++) {
@@ -142,7 +150,37 @@ char get_winning_opportunity(map_data * map, int needed_char, unsigned char goal
                 return get_char_from_int(zero_pos);
             }
 
-                
+            
+            zero_pos = 0;
+            // check \e
+            if(i-goal+1 >= 0 && j-goal-1 >= 0) {
+                for(int k=0; k < goal; k++) {
+                    if(map->pMap[i-k][j-k] != needed_char + '0') {
+                        if(map->pMap[i-k][j-k] == 0 + '0') { // if the number is 0, this is good, and we save thats position (but we will let only one from them to exist)
+                            if(k > 0) {
+                                if(map->pMap[i-k+1][j-k] == 0 + '0') {
+                                    zero_counter = 0;
+                                    break;
+                                } else { // if there is not void underneath the chosen number
+                                    zero_pos = j-k;
+                                    zero_counter++;
+                                }
+                            }
+                        } else { // or if it is not zero, this means this is already taken for an other user
+                            zero_counter = 0;
+                            break; // with this situation, we dont need to continue the investigating
+                        }
+                    }
+                }
+
+                if(zero_counter == 1) { // we want to see only one zero! and that could be anywhere in the sequence (so this is why we saved that position)
+                    c = goal;
+                }
+            }
+
+            if(c == goal) {
+                return get_char_from_int(zero_pos);
+            }
 
         }
     }
@@ -152,42 +190,54 @@ char get_winning_opportunity(map_data * map, int needed_char, unsigned char goal
 
 char ai_main(map_data * map, unsigned short round, unsigned char goal) {
     char result;
-    
+    int random_start_pos;
+
+    int loop_c = 0;
+
     printf("from ai > goal: %hhu\n", goal);
 
     unsigned short user_id = (round + 1) % 2;
     int current_point_count = get_user_point_count(map, user_id+1);
     
     if(current_point_count > 0) {
-        // atm both has at least a point on the map
+        while(true) {
+            if(loop_c > 20) {
+                printf("The table is full!\n");
+                break;
+            }
+            // atm both has at least a point on the map
 
-        // check for win positions
-        char win_option = get_winning_opportunity(map, user_id+1, goal);
-        printf("win option: %c\n", win_option);
+            // check for win positions
+            char win_option = get_winning_opportunity(map, user_id+1, goal);
+            printf("win option: %c\n", win_option);
 
-        if(win_option != '0') {
-            return win_option;
+            if(win_option != '0') {
+                return win_option;
+            }
+
+            // check for win positions for the enemy
+            win_option = get_winning_opportunity(map, 1-user_id+1, goal);
+            printf("kill streak option: %c\n", win_option);
+
+            if(win_option != '0') {
+                return win_option;
+            }
+
+            // if there is no win position nether for the ai and the user, than make a point what has benefits for the ai
+            // follow its longest line or if that is not completeable then start a new random point
+
+            random_start_pos = random_move(map->y); // here this is not start pos
+            if(empty_pos(map, 0, random_start_pos)) { break; }
+
+            loop_c++;
         }
 
-        // check for win positions for the enemy
-        win_option = get_winning_opportunity(map, 1-user_id+1, goal);
-        printf("kill streak option: %c\n", win_option);
-
-        if(win_option != '0') {
-            return win_option;
-        }
-
-        // if there is no win position nether for the ai and the user, than make a point what has benefits for the ai
-        // follow its longest line or if that is not completeable then start a new random point
-
-        result = get_char_from_int(random_move(map->x));
+        result = get_char_from_int(random_start_pos);
 
     } else {
-        int random_start_pos;
-        
         while(true) { // refactor this
-            random_start_pos = random_move(map->x);
-            if(empty_pos(map, map->y-1, random_start_pos)) { break; }
+            random_start_pos = random_move(map->y);
+            if(empty_pos(map, map->x-1, random_start_pos)) { break; }
         }
 
         result = get_char_from_int(random_start_pos);
